970f79f1b5a0245ac5c1853e87b55864aa925391
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2025-04-13 20:58:59 -0300

    Encode Run Length

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "ttees";
+        String s = "teste";
         System.out.println(encodeRunLengths(s));
     }
 
